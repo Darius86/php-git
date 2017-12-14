19245418097867796856410807a3771045d28ee6
Darius86 <32070918+Darius86@users.noreply.github.com>
2017-12-14 20:06:46 +0200

    pakeitimai funkciju

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
 <?php
-function arrayAverage($array){
-    $suma=0;
-    for($i=0; $i<count($array);$i++){
-        $suma+=$array[$i];
+$array=[['jonas, jonaitis,jonasmail,],[antanas, antanaitis, antanmaail,],[petras, petraitis, petrasmail']];
+function peopleInfo(array $array){
+    for ($i=0; $i<count($array); $i++) {
+        echo '<tr>';
+        for ($j=0; $j<count($array[$i]); $j++){
+            echo '<td>' . $array[$i][$j] . '</td>';
+        }
+        echo '</tr>';
     }
-    $average=$suma/count($array);
-    return $average;
 }
-$array=[1,2,4];
-echo arrayAverage($array);
+
+
+
+
+
+
+
 ?>
+
+
+<table>
+    <tr>
+        <th>Vardas</th>
+        <th>Pavarde</th>
+        <th>El.pastas</th>
+    </tr>
+    <?php
+    peopleInfo($array);
+    ?>
+
+</table>
